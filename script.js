6c88ae9913f3e4a9025cbb789bea3ac40b8c0504
Uri Livshits <Yuri@MacBook-Pro.local>
2020-09-20 00:28:05 +0300

    adding starting project, learning git

---
+++ script.js
@@ -0,0 +1,43 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient () {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background;
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+body.style.background = "linear-gradient(to right, #0C1BED, #A7C318)";
+css.textContent = body.style.background;
+
+
+//CREATING RANDOMIZE BUTTON
+var randomBtn = document.createElement("button");
+body.append(randomBtn);
+randomBtn.innerHTML = "Randomize";
+randomBtn.classList.add("randomBtn");
+
+randomBtn.addEventListener("click", () => {
+    var c1 = Math.floor(Math.random() * 255);
+    var c2 = Math.floor(Math.random() * 255);
+    var c3 = Math.floor(Math.random() * 255);
+    var c4 = Math.floor(Math.random() * 255);
+    var c5 = Math.floor(Math.random() * 255);
+    var c6 = Math.floor(Math.random() * 255);
+
+    body.style.background = body.style.background = `linear-gradient(to right, rgb(${c1}, ${c2}, ${c3}), rgb(${c4}, ${c5}, ${c6}))`;
+    css.textContent = body.style.background;
+});
+
+// color1.addEventListener("input", function () {
+//     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+// });
+
+// color2.addEventListener("input", function () {
+//     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+// });
+
